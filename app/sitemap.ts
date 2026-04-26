81b0db3c1baf424d28915627f3f9b74304d5e3a1
Richo Saputra <m.richo.saputra69@gmail.com>
2026-04-26 23:54:07 +0700

    feat: add sitemap.xml generation for SEO optimization

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://fortisandco.my.id',
+      url: 'https://fortisandco.my.id/',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 1,
